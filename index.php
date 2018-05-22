0c73085169f8cfc7da8dbaf493dc59cbb816304a
동현\동현 <ms7045436@likelion.org>
2018-05-22 16:49:37 +0900

    add login form

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE HTML>
+<?php session_start(); ?>
 <html>
   <head>
     <title>
@@ -19,6 +20,9 @@
     <!--font-->
     <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:700" rel="stylesheet">
 
+    <!--icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <style>
       body{
         font-family: 'Nanum Gothic', sans-serif;
@@ -38,7 +42,15 @@
       <nav>
         <ul>
           <li id = 'logo'><a href = 'index.html'>logo</a></li>
-          <li style='float: right' id = 'login'><a href = 'login.html'>login</a></li>
+          <?php
+          if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
+            echo "<li style='float: right' id = 'login'><a href = 'login.php'><i class = 'material-icons'>person</i></a></li>";
+          } else {
+            $user_id = $_SESSION['user_id'];
+            $user_name = $_SESSION['user_name'];
+            echo "<p><strong>$user_name</strong>";
+          }
+          ?>
         </ul>
       </nav>
     </header>
@@ -53,10 +65,10 @@
     <footer>
       <nav>
         <ul style='margin-bottom: 0;'>
-          <li id = 'find'><a href = 'find.html'>가게찾기</a></li>
-          <li id = 'review'><a href = 'review.html'>리뷰</a></li>
-          <li id = 'mypage'><a>마이페이지</a></li>
-          <li id = 'more' style = 'border-right: 0;'><a>더보기</a></li>
+          <li id = 'find'><a href = 'find.html'><i class='material-icons'>pageview</i></a></li>
+          <li id = 'review'><a href = 'review.html'><i class='material-icons'>assignment</i></a></li>
+          <li id = 'mypage'><a><i class='material-icons'>info</i></a></li>
+          <li id = 'more' style = 'border-right: 0;'><a><i class='material-icons'>more</i></a></li>
         </ul>
       </nav>
     </footer>
